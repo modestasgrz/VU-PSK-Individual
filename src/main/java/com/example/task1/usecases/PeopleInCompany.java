d68d23e791f120f6dcdfe425a5c32d515b3c615c
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2022-05-09 23:49:28 +0300

    OptLockExceptionStuff

--- src/main/java/com/example/task1/usecases/PeopleInCompany.java
+++ src/main/java/com/example/task1/usecases/PeopleInCompany.java
@@ -4,9 +4,23 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
 import javax.transaction.Transactional;
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.Authenticator;
+import java.net.CookieHandler;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 import com.example.task1.decorators.Note;
 import lombok.Getter;
@@ -53,5 +67,30 @@ public class PeopleInCompany implements Serializable {
 
     public String loadNote() {
         return note.displayNote();
+    }
+
+    @Transactional
+    public void createOptLockException() {
+        personToCreate.setCompany(this.company);
+        personToCreate.setName("Minerva McGonagall");
+        personToCreate.setSalary(5000.0);
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/task1/rest/people/"))
+                .POST(HttpRequest.BodyPublishers.ofString("{\n" +
+                        "    \"name\": \"Minerva McGonagall\",\n" +
+                        "    \"salary\": 5000.0,\n" +
+                        "    \"companyName\": \"Company1\"\n" +
+                        "}"))
+                .build();
+        try {
+            HttpResponse<?> response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
+            System.out.println("Created request with response code: " + response.statusCode());
+        }
+        catch (Exception e) {
+            System.out.println("Exception occured during sending the create person response" + e.getMessage());
+        }
+        peopleDAO.persist(personToCreate);
     }
 }
